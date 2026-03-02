111bebf4f734c83fd5698bea6eb431c048856c6f
aasimdev <aasimdev0@gmail.com>
2026-03-03 03:36:01 +0500

    tariq commit

--- client/components/pages/welcome/TemplatesSlider.vue
+++ client/components/pages/welcome/TemplatesSlider.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="mx-auto max-w-266">
-    <div class="max-w-lg mx-auto text-center md:px-1">
+  <div class="mx-auto mb-12 max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-lg text-center">
       <!-- <p class="text-lg font-semibold leading-8 tracking-tight text-blue-500">
         Single or multi-page forms
       </p> -->
       <h2
-        class="text-3xl sm:text-5xl sm:leading-14 font-semibold text-gray-950 tracking-[-1%]"
+        class="text-3xl sm:text-5xl sm:leading-14 font-semibold text-neutral-900 tracking-[-1%]"
       >
-        Templates for financial services
+        Templates for <br class="hidden sm:block" />
+        financial services
       </h2>
       <p
-        class="mt-4 text-base font-normal text-gray-600 leading-7 tracking-[-1.1%]"
+        class="mt-4 text-base font-normal text-neutral-500 leading-7 tracking-[-1.1%]"
       >
-        All templates are fully customizable — adapt them to your compliance and
-        brand requirements in minutes.
+        If you need inspiration, checkout our templates.
       </p>
     </div>
     <!-- <div class="my-3 flex justify-center">
@@ -45,7 +45,7 @@
       >
         <li
           v-for="template in sliderTemplates"
-          :key="template.name"
+          :key="template.id"
           class="mx-4 w-72 h-auto"
         >
           <single-template :template="template" />
@@ -62,74 +62,38 @@
 </template>
 
 <script>
-import SingleTemplate from "../templates/SingleTemplate.vue"
+import SingleTemplate from "../templates/SingleTemplate.vue";
 
 export default {
   components: { SingleTemplate },
   setup() {
-    const { list } = useTemplates()
-    const { data: templates } = list({ limit: 10 })
+    const { list } = useTemplates();
+    const { data: templates } = list({ limit: 10 });
 
     return {
-      sliderTemplates: computed(() => {
-        if (templates.value && templates.value.length) {
-          return templates.value
-        }
-
-        return [
-          {
-            name: "KYC / Client Onboarding Form",
-            slug: "kyc-client-onboarding-1",
-            short_description: "Some text goes here...",
-          },
-          {
-            name: "Loan Application Form",
-            slug: "loan-application-form",
-            short_description: "Some text goes here...",
-          },
-          {
-            name: "Internal Audit Checklist",
-            slug: "internal-audit-checklist",
-            short_description: "Some text goes here...",
-          },
-          {
-            name: "Expense Reimbursement Form",
-            slug: "expense-reimbursement-form",
-            short_description: "Some text goes here...",
-          },
-          {
-            name: "Client Feedback Form",
-            slug: "client-feedback-form",
-            short_description: "Some text goes here...",
-          },
-          {
-            name: "Some title",
-            slug: "some-title",
-            short_description: "Some text goes here...",
-          },
-        ]
-      }),
-    }
+      sliderTemplates: computed(() => templates.value ?? []),
+    };
   },
 
-  // watch: {
-  //   sliderTemplates: {
-  //     deep: true,
-  //     handler() {
-  //       this.$nextTick(() => {
-  //         this.setInfinite();
-  //       });
-  //     },
-  //   },
-  // },
-  // methods: {
-  //   setInfinite() {
-  //     const ul = this.$refs["templates-slider"];
-  //     if (!ul || ul.nextSibling) return;
+  watch: {
+    sliderTemplates: {
+      deep: true,
+      handler() {
+        this.$nextTick(() => {
+          this.setInfinite();
+        });
+      },
+    },
+  },
 
-  //     ul.insertAdjacentHTML("afterend", ul.outerHTML);
-  //     ul.nextSibling.setAttribute("aria-hidden", "true");
-  //   },
-  // },
-}
+  methods: {
+    setInfinite() {
+      const ul = this.$refs["templates-slider"];
+      if (ul) {
+        ul.insertAdjacentHTML("afterend", ul.outerHTML);
+        ul.nextSibling.setAttribute("aria-hidden", "true");
+      }
+    },
+  },
+};
 </script>
